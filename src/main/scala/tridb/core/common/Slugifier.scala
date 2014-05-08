2687c90d682faa65e0adbca3c7ace36e2eb89838
Pedro Santos <donbonifacio@gmail.com>
2014-05-08 21:43:39 +0100

    Slugify working

--- src/main/scala/tridb/core/common/Slugifier.scala
+++ src/main/scala/tridb/core/common/Slugifier.scala
@@ -5,7 +5,7 @@ import java.text._
 
 object Slugifier {
 
-  def slugify(raw : String) = raw
+  def slugify(raw : String) = decompose(raw.toLowerCase.replaceAll(" ", "-"))
 
   def decompose(raw : String) = {
     Normalizer.normalize(raw, Normalizer.Form.NFKD).replaceAll("\\p{InCombiningDiacriticalMarks}+","")
--- src/test/scala/tridb/core/common/SlugifierSpec.scala
+++ src/test/scala/tridb/core/common/SlugifierSpec.scala
@@ -5,6 +5,22 @@ import net.tridb.core.common.Slugifier._
 
 class SlugifierSpec extends UnitSpec {
 
+  describe("slugify") {
+
+    it("downcases the string") {
+      slugify("Pedro") should equal("pedro")
+    }
+
+    it("replaces spaces with dashes") {
+      slugify("pedro santos") should equal("pedro-santos")
+    }
+
+    it("completly formats as slug a given string") {
+      slugify("Patrícia Simões") should equal("patricia-simoes")
+    }
+
+  }
+
   describe("decompose") {
 
     it("handles basic examples") {
